2127d73764f4ae5b8bc52b4eb109cde5f363ef37
shubhamrajput768 <shubhamr1938@gmail.com>
2023-11-16 22:26:04 +0530

    python project

---
+++ login/info.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangologinsystem@gmail.com'
+EMAIL_HOST_PASSWORD = 'ucxhrptgkupzjpga'
+EMAIL_PORT = 587
